41a66d0b0297753849565a0f2799f1e6c41987ab
Roman-Popov-2021 <roman.popov121212@gmail.com>
2024-10-12 21:08:41 +0300

    Update, getting ip user.

--- AppServerDocker/Program.cs
+++ AppServerDocker/Program.cs
@@ -2,36 +2,18 @@
 using System.Net.Sockets;
 using System.Text;
 
-// Порт для прослушивания сообщений
-const int LISTEN_PORT = 13400;
-// Порт для отправления сообщений
-const int SEND_PORT = 13400;
+UdpClient udpListener = new UdpClient(8000);
 
-// Сокет для работы по сети
-using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-
-// Точка для прослушивания сообщений
-IPEndPoint ipListen = new IPEndPoint(IPAddress.Any, LISTEN_PORT);
-
-// Начинаем слушать входящие сообщения
-socket.Bind(ipListen);
-
-// Метод для прослушивания
-static async Task ListenMessage(Socket socket, IPEndPoint ipListen)
+// Сервер прослушивает
+while (true)
 {
-    while (true)
-    {
-        // Данные для чтения
-        byte[] data = new byte[256];
-        // Получаем данные от клиента
-        await socket.ReceiveFromAsync(data, ipListen);
+    IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    byte[] receivedBytes = udpListener.Receive(ref remoteEndPoint);
 
-        Console.WriteLine(Encoding.Unicode.GetString(data).Replace("\0", string.Empty));
+    string result = Encoding.UTF8.GetString(receivedBytes);
 
-        string answer = "Получил!";
-        Console.WriteLine($"Ip user: {socket.RemoteEndPoint}");
-        await socket.SendToAsync(Encoding.Unicode.GetBytes(answer), socket.RemoteEndPoint);
-    }
+    string ip = remoteEndPoint.Address.ToString();
+    Console.WriteLine($"Your ip: {ip}\nData: {result}");
+
+    udpListener.Send(Encoding.Unicode.GetBytes(ip), remoteEndPoint);
 }
-
-await Task.Factory.StartNew(() => ListenMessage(socket, ipListen));
